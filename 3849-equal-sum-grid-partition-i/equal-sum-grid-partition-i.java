7b24d8077fe08426e060c7bba5cbcd1c0f301f96
Diya Bajaj <113410958+Diyu04@users.noreply.github.com>
2026-03-25 21:57:52 +0530

    Time: 4 ms (97.20%) | Memory: 161.7 MB (33.57%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
@@ -1,29 +1,34 @@
 class Solution {
     public boolean canPartitionGrid(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        long total=0;
+        long total = 0;
         for (int[] row : grid)
             for (int x : row)
                 total += x;
 
-                if((total&1) == 1) return false;  // odd
+        if ((total & 1) == 1)
+            return false; // odd
 
-                long target = total/2, sum=0;
+        long target = total / 2, sum = 0;
 
-                for(int i=0;i<m-1;i++){
-                    for(int j=0;j<n;j++){
-                        sum+=grid[i][j];
-                        if(sum==target) return true;
-                    }
-                }
-                sum=0;
+        // horizontal cut
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n; j++) {
+                sum += grid[i][j];
+                if (sum == target)
+                    return true;
+            }
+        }
+        sum = 0;
 
-                 for (int j = 0; j < n - 1; j++) {
+        // veritcal cut
+        for (int j = 0; j < n - 1; j++) {
             for (int i = 0; i < m; i++)
                 sum += grid[i][j];
-            if (sum == target) return true;
+            if (sum == target)
+                return true;
         }
-        
+
         return false;
 
     }
