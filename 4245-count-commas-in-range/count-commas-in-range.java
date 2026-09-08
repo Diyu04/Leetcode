850272dbed79b278b797126b1785f48df706847f
Diya Bajaj <113410958+Diyu04@users.noreply.github.com>
2026-09-08 22:36:07 +0530

    Time: 8 ms (8.47%) | Memory: 42.8 MB (8.32%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,19 +1,11 @@
 class Solution {
     public int countCommas(int n) {
-    // 1–3 digits → 0 commas
-    // 4–6 digits → 1 comma
-    // 7–9 digits → 2 commas
-    // 1-999  -> no comma
-    // 1,000  1,001  1,002 ...
-        int ans =0;
-        int start=1000;
-        int commas=1;
-        
-        while(start<=n){
-            ans+=(n-start+1)*commas;
-            start*=1000;
-            commas++;
+         int res = 0;
+        for (int a = 1; a <= n; ++a) {
+            if (a > 999) {
+                res += 1;
+            }
         }
-        return ans;
+        return res;
     }
 }
